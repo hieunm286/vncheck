88a2a2cec4284362f69061806f04c1599fd1fe22
NandoShinji <ng.minh.hieu.286@gmail.com>
2020-10-18 12:21:30 +0700

    Fix tsx agency

--- src/app/pages/agency/agency-management/agency-ui-context.tsx
+++ src/app/pages/agency/agency-management/agency-ui-context.tsx
@@ -1,93 +1,96 @@
-import React, {createContext, useContext, useState, useCallback} from 'react';
-import {isEqual, isFunction} from 'lodash';
-import {initialFilter} from './agency-ui-helpers';
+import React, { createContext, useContext, useState, useCallback } from 'react';
+import { isEqual, isFunction } from 'lodash';
+import { initialFilter } from './agency-ui-helpers';
 
 const AgencyUIContext = createContext<any>(null);
 
 export function useAgencyUIContext() {
-    return useContext(AgencyUIContext);
+  return useContext(AgencyUIContext);
 }
 
 export const AgencyUIConsumer = AgencyUIContext.Consumer;
 
-export function AgencyUIProvider({agencyUIEvents, children}: any) {
-    const [queryParams, setQueryParamsBase] = useState(initialFilter);
-    const [ids, setIds] = useState([]);
-    const setQueryParams = useCallback(nextQueryParams => {
-        setQueryParamsBase(prevQueryParams => {
-            if (isFunction(nextQueryParams)) {
-                nextQueryParams = nextQueryParams(prevQueryParams);
-            }
+export function AgencyUIProvider({ agencyUIEvents, children }: any) {
+  const [queryParams, setQueryParamsBase] = useState(initialFilter);
+  const [ids, setIds] = useState([]);
+  const setQueryParams = useCallback(nextQueryParams => {
+    setQueryParamsBase(prevQueryParams => {
+      if (isFunction(nextQueryParams)) {
+        nextQueryParams = nextQueryParams(prevQueryParams);
+      }
 
-            if (isEqual(prevQueryParams, nextQueryParams)) {
-                return prevQueryParams;
-            }
+      if (isEqual(prevQueryParams, nextQueryParams)) {
+        return prevQueryParams;
+      }
 
-            return nextQueryParams;
-        });
-    }, []);
+      return nextQueryParams;
+    });
+  }, []);
 
-    const useAutofill = false;
+  const useAutofill = false;
 
-    let initAgency = null;
+  let initAgency = null;
 
-    if (useAutofill) {
-        initAgency = {
-            // id: undefined,
-            agency_id: '',
-            name: 'aaa',
-            status: 0,
-            phone: '0214367589',
-            tax_id: 'aaa',
-            username: 'aaa',
-            agency_type_id: 'aaa',
-            shipping_address: 'aaa',
-            address: '',
-            country: '',
-            town: '',
-            city: '',
-            district: '',
-            state: '',
-            email: 'aaa@gmail.com',
-            owner_phone: '0123456789',
-            agency_image: '',
-        };
-    } else {
-        initAgency = {
-            // id: undefined,
-            agency_id: '',
-            name: '',
-            status: 0,
-            phone: '',
-            tax_id: '',
-            username: '',
-            agency_type_id: '',
-            shipping_address: '',
-            address: '',
-            country: '',
-            town: '',
-            city: '',
-            district: '',
-            state: '',
-            email: '',
-            owner_phone: '',
-            agency_image: '',
-        };
-    }
-
-    const value: any = {
-        queryParams,
-        setQueryParamsBase,
-        ids,
-        setIds,
-        setQueryParams,
-        initAgency,
-        newAgencyButtonClick: agencyUIEvents.newAgencyButtonClick,
-        openEditAgencyDialog: agencyUIEvents.openEditAgencyDialog,
-        openDeleteAgencyDialog: agencyUIEvents.openDeleteAgencyDialog,
-        openDetailAgencyDialog: agencyUIEvents.openDetailAgencyDialog,
-        openDeleteManyAgencyDialog: agencyUIEvents.openDeleteManyAgencyDialog,
+  if (useAutofill) {
+    initAgency = {
+      // id: undefined,
+      agency_id: '',
+      name: 'aaa',
+      status: 0,
+      phone: '0214367589',
+      tax_id: 'aaa',
+      username: 'aaa',
+      agency_type_id: 'aaa',
+      shipping_address: 'aaa',
+      address: '',
+      country: '',
+      town: '',
+      city: '',
+      district: '',
+      state: '',
+      email: 'aaa@gmail.com',
+      owner_phone: '0123456789',
+      agency_image: '',
     };
+  } else {
+    initAgency = {
+      // id: undefined,
+      agency_id: '',
+      name: '',
+      status: 0,
+      phone: '',
+      tax_id: '',
+      owner_name: '',
+      agency_type_id: '',
+      shipping_address: '',
+      agency_addresses: {
+        address: '',
+        town: '',
+        city: '',
+        district: '',
+        state: '',
+      },
+      country: '',
+      address: '',
+      email: '',
+      owner_phone: '',
+      agency_image: '',
+    };
+  }
 
-    return <AgencyUIContext.Provider value={value}> {children} </AgencyUIContext.Provider>;
+  const value: any = {
+    queryParams,
+    setQueryParamsBase,
+    ids,
+    setIds,
+    setQueryParams,
+    initAgency,
+    newAgencyButtonClick: agencyUIEvents.newAgencyButtonClick,
+    openEditAgencyDialog: agencyUIEvents.openEditAgencyDialog,
+    openDeleteAgencyDialog: agencyUIEvents.openDeleteAgencyDialog,
+    openDetailAgencyDialog: agencyUIEvents.openDetailAgencyDialog,
+    openDeleteManyAgencyDialog: agencyUIEvents.openDeleteManyAgencyDialog,
+  };
+
+  return <AgencyUIContext.Provider value={value}> {children} </AgencyUIContext.Provider>;
 }
