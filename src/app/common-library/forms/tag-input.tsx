bd6e63a3184e013db3e59e1c752c3d9a8b5d5975
nam <namtp185@gmail.com>
2020-12-08 16:57:52 +0700

    fix: common GUI: missing validation tooltip

--- src/app/common-library/forms/tag-input.tsx
+++ src/app/common-library/forms/tag-input.tsx
@@ -19,7 +19,7 @@ const getFieldCSSClasses = (touched: any, errors: any) => {
 
   if (touched && errors) classes.push('is-invalid');
 
-  if (touched && errors) classes.push('is-valid');
+  if (touched && !errors) classes.push('is-valid');
 
   return classes.join(' ');
 };
