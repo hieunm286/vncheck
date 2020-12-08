61d0a481105cb9d1738bd84c36f94b861dacb1bf
nam <namtp185@gmail.com>
2020-12-08 18:12:07 +0700

    fix: menu icon

--- src/app/layout/components/aside/aside-menu/aside-menu-data.tsx
+++ src/app/layout/components/aside/aside-menu/aside-menu-data.tsx
@@ -22,8 +22,14 @@ export const MenuItems: MenuItemModel[] = [
           <SVG src={ToAbsoluteUrl('/media/svg/vncheck/account.svg')} style={{ width: '17px' }} />
         ),
       },
-      { title: 'MENU.USER.ROLE', url: '/account/role', icon: 'role.svg' },
-      { title: 'MENU.USER.ORGANIZATION', url: '/account/organization', icon: 'role.svg' },
+      { 
+        title: 'MENU.USER.ROLE', 
+        url: '/account/role', 
+        icon: 'role.svg' },
+      { 
+        title: 'MENU.USER.ORGANIZATION', 
+        url: '/account/organization', 
+        icon: 'organization.svg' },
     ],
   },
   // {
@@ -49,79 +55,68 @@ export const MenuItems: MenuItemModel[] = [
       {
         title: 'MENU.DATA.CATEGORY.BATCH',
         url: '/land-lot',
-        icon: <ImageOutlinedIcon style={{ width: '17px' }} htmlColor="#888C9F" />,
+        // icon: <ImageOutlinedIcon style={{ width: '17px' }} htmlColor="#888C9F" />,
+        icon: 'land-lot.svg',
       },
-      {
-        title: 'MENU.DATA.CATEGORY',
-        url: '/category',
-        icon: 'product.svg',
-        children: [
-          {
-            title: 'MENU.DATA.CATEGORY.PRODUCT',
-            url: '/category/PRODUCT',
-            icon: <FormatListBulletedIcon style={{ width: '17px' }} htmlColor="#888C9F" />,
-          },
-          { title: 'MENU.DATA.CATEGORY.CATEGORY', url: '/category/CATEGORY', icon: 'category.svg' },
-          {
-            title: 'MENU.DATA.CATEGORY.BASE_UNIT',
-            url: '/category/basic-unit',
-            icon: 'base-unit.svg',
-          },
-          {
-            title: 'MENU.DATA.CATEGORY.CONVERT_UNIT',
-            url: '/category/CONVERT_UNIT',
-            icon: <SyncOutlinedIcon htmlColor="#888C9F" />,
-          },
-        ],
-      },
-      {
-        title: 'MENU.DATA.AGENCY',
-        url: '/agency',
-        icon: 'distributor-1.svg',
-        children: [
-          {
-            title: 'MENU.DATA.AGENCY.AGENCY',
-            url: '/agency',
-            icon: 'distributor-2.svg',
-          },
-          {
-            title: 'MENU.DATA.AGENCY.AGENCY_TYPE',
-            url: '/agency-type',
-            icon: 'distributor-type.svg',
-          },
-        ],
-      },
+      // {
+      //   title: 'MENU.DATA.CATEGORY',
+      //   url: '/category',
+      //   icon: 'product.svg',
+      //   children: [
+      //     {
+      //       title: 'MENU.DATA.CATEGORY.PRODUCT',
+      //       url: '/category/PRODUCT',
+      //       icon: <FormatListBulletedIcon style={{ width: '17px' }} htmlColor="#888C9F" />,
+      //     },
+      //     { title: 'MENU.DATA.CATEGORY.CATEGORY', url: '/category/CATEGORY', icon: 'category.svg' },
+      //     {
+      //       title: 'MENU.DATA.CATEGORY.BASE_UNIT',
+      //       url: '/category/basic-unit',
+      //       icon: 'base-unit.svg',
+      //     },
+      //     {
+      //       title: 'MENU.DATA.CATEGORY.CONVERT_UNIT',
+      //       url: '/category/CONVERT_UNIT',
+      //       icon: <SyncOutlinedIcon htmlColor="#888C9F" />,
+      //     },
+      //   ],
+      // },
       {
         title: 'MENU.DATA.PRODUCT',
         url: '/product',
-        icon: 'distributor-1.svg',
+        icon: 'species-categories.svg',
         children: [
           {
             title: 'MENU.DATA.PRODUCT.PRODUCT_TYPE',
             url: '/product-type',
-            icon: 'distributor-2.svg',
+            icon: 'species.svg',
           },
           {
             title: 'MENU.DATA.PRODUCT.PACK',
             url: '/product-packaging',
-            icon: 'distributor-type.svg',
+            icon: 'product-packaging.svg',
           },
         ],
       },
       {
         title: 'MENU.DATA.DISTRIBUTION',
         url: '/distribution',
-        icon: 'distributor-1.svg',
+        icon: 'distribution.svg',
         children: [
           {
             title: 'MENU.DATA.DISTRIBUTION.MULTILEVEL_SALE',
             url: '/multilevel-sale',
-            icon: 'distributor-2.svg',
+            icon: 'multilevel-sale.svg',
+          },
+          {
+            title: 'MENU.DATA.DISTRIBUTION.AGENCY',
+            url: '/agency',
+            icon: 'agency.svg',
           },
           {
             title: 'MENU.DATA.DISTRIBUTION.SHIPPING_AGENCY',
             url: '/shipping-agency',
-            icon: 'distributor-2.svg',
+            icon: 'shipping-agency.svg',
           },
         ],
       },
