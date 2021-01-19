dd6fdc94c6e71638e3158969a229195c11068552
nam <namtp185@gmail.com>
2021-01-19 17:07:58 +0700

    feat: master role: clone role

--- src/app/pages/role/role.model.tsx
+++ src/app/pages/role/role.model.tsx
@@ -1,3 +1,13 @@
 export type RoleModel = {
-  _id: string;
+  _id?: string;
+  parent?: string;
+  name: string;
+  managementUnit: {
+    _id?: string;
+    code?: string;
+    level?: number;
+    parent?: string;
+  } | string | undefined;
+  scopes: string[];
+  status: string;
 }
