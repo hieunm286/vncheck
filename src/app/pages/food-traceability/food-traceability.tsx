7ced9658de96411c25b9fdbd2052d9e4f8041279
NandoShinji <ng.minh.hieu.286@gmail.com>
2021-02-22 10:18:21 +0700

    phan quyen xem truy xuat

--- src/app/pages/food-traceability/food-traceability.tsx
+++ src/app/pages/food-traceability/food-traceability.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import { RootStateOrAny, shallowEqual, useDispatch, useSelector } from 'react-redux';
+import { RenderInfoDetail, RenderInfoDetailColumn } from '../../common-library/common-types/common-type';
 import { AuthPage } from '../auth';
 import { ProductionPlanDetail } from '../production-plan/production-plan-detail';
 import { entityExample, exampleDetail } from './example-data';
 import { GetDetail } from './food-traceability.service';
+import { GetById } from './food-traceability.service';
+
+const getExistInfo = (target: string[], current: RenderInfoDetailColumn) => {
+  const viewData: RenderInfoDetailColumn = {}
+
+  Object.keys(current).forEach(role => {
+    if (target.includes(role)) {
+      console.log('1')
+      viewData[role] = current[role]
+    }
+  })
+
+  return viewData
+}
 
 function FoodTraceability({ id }: { id?: string }) {
   const [entity, setEntity] = React.useState<any>();
-
+  const [viewDetail, setViewDetail] = React.useState<RenderInfoDetail>(exampleDetail)
   const { auth } = useSelector((state: RootStateOrAny) => ({ auth: state.auth }), shallowEqual);
 
   React.useEffect(() => {
@@ -22,6 +37,33 @@ function FoodTraceability({ id }: { id?: string }) {
     }
   }, [auth, id]);
 
+  React.useEffect(() => {
+    if (auth?._id) {
+      GetById(auth._id).then(res => {
+        const userRole: string[] = res.data?.addedScope?.enable ?? []
+        const viewInfo: RenderInfoDetail = exampleDetail.map(item => {
+          return ({
+          ...item,
+          data: getExistInfo(userRole, item.data)
+        })})
+
+        console.log(viewInfo)
+        setViewDetail(viewInfo)
+      }).catch(err => {
+        console.log(err)
+      })
+    } else {
+      const viewInfo: RenderInfoDetail = exampleDetail.map(item => {
+        return ({
+        ...item,
+        data: getExistInfo([], item.data)
+      })})
+
+      console.log(viewInfo)
+      setViewDetail(viewInfo)
+    }
+  }, [auth, id])
+
   console.log(entity);
 
   return (
@@ -30,7 +72,7 @@ function FoodTraceability({ id }: { id?: string }) {
       {auth?._id ? 'Đã đăng nhập' : 'Chưa đăng nhập'}
       <h4>{!auth?._id && 'Vui lòng đăng nhập để xem nhiều thông tin hơn'}</h4>
       <div className="w-md-75 w-lg-50 mx-auto">
-        <ProductionPlanDetail entity={entity} renderInfo={exampleDetail} showComment={false} />
+        <ProductionPlanDetail entity={entity} renderInfo={viewDetail} showComment={false} />
       </div>
     </>
   );
